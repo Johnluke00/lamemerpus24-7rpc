23aa9bf0a41590e7f175b28c6ac7931984cbed6b
John Luke <145742936+Johnluke00@users.noreply.github.com>
2024-11-08 07:03:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,1 @@
-const express = require('express');
-const server = express();
- 
-server.all('/', (req, res) => {
-  res.send(`Go to https://console.cron-job.org/dashboard and paste this link to ping it every 1 minute 💞`)
-})
- 
-function keepAlive() {
-  server.listen(3000, () => { console.log("RPC is Ready 💞" + Date.now()) });
-}
- 
-module.exports = keepAlive;
 
